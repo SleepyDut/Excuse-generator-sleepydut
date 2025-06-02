10c883202574f57cf49e032095de3a6f1b1cc655
SleepyDut <diegoundreiner@gmail.com>
2025-06-02 22:14:37 +0000

    Creación de excusas

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import "bootstrap";
 import "./style.css";
 
+let who = ['The dog ', 'My grandma ', 'The mailman ', 'My bird '];
+let action = ['ate ', 'peed ', 'crushed ', 'broke '];
+let what = ['my homework ', 'my phone ', 'the car '];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
@@ -8,4 +13,5 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector('.alert').innerHTML = who [Math.floor(Math.random()*4)] + action [Math.floor(Math.random()*4)] + what [Math.floor(Math.random()*4)] + when [Math.floor(Math.random()*4)]
 };
