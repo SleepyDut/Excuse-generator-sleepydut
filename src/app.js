70c5ef638c8bda77737415813fda0b836eadd2f3
SleepyDut <diegoundreiner@gmail.com>
2025-06-05 00:14:53 +0000

    refactorizacion de código y buenas practicas

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ let action = ['ate ', 'peed ', 'crushed ', 'broke '];
 let what = ['my homework ', 'my phone ', 'the car '];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let randomWho = who [Math.floor(Math.random()*4)]
+let randomAction = action [Math.floor(Math.random()*4)]
+let randomWhat = what [Math.floor(Math.random()*3)]
+let randomWhen = when [Math.floor(Math.random()*5)]
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-  document.querySelector('.alert').innerHTML = who [Math.floor(Math.random()*4)] + action [Math.floor(Math.random()*4)] + what [Math.floor(Math.random()*4)] + when [Math.floor(Math.random()*4)]
+  document.querySelector('.alert').innerHTML = `${randomWho}  ${randomAction}  ${randomWhat}  ${randomWhen}`
 };
